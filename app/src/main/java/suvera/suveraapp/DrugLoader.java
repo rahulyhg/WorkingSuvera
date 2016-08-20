177c9e13505ec533298c0d9ecc732386ebce5a8d
William Meaton <will.meaton@gmail.com>
2016-08-20 12:47:59 +0100

    added basic search function

--- app/src/main/java/suvera/suveraapp/DrugLoader.java
+++ app/src/main/java/suvera/suveraapp/DrugLoader.java
@@ -73,7 +73,17 @@ public class DrugLoader {
             Log.e("JSON", e.getMessage());
         }
     }
-    
+
+    public ArrayList<Drug> search(String search){
+        ArrayList<Drug> result = new ArrayList<Drug>();
+        for(Drug d : drugsList){
+            if(d.getName().contains(search)){
+                result.add(d);
+            }
+        }
+        return result;
+    }
+
     public ArrayList<Drug> getDrugsList() {
         return drugsList;
     }
