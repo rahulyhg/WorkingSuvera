b56e2dc1d0679f8885724c80f201ab638005877d
William Meaton <will.meaton@gmail.com>
2016-08-28 22:21:32 +0100

    Added AddDrug activity

---
+++ app/src/main/java/suvera/suveraapp/AddDrug.java
@@ -0,0 +1,13 @@
+package suvera.suveraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDrug extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_drug);
+    }
+}
